67b02fa16f9b70f22e063bdfae66e94ae42a8140
DianaAyala2 <00021325@uca.edu.sv>
2025-06-23 20:59:14 -0600

    matriz base 2

--- nivel1.cpp
+++ nivel1.cpp
@@ -12,4 +12,21 @@ void gLabyrinth(int rows, int columns, int density) //base function to adjust th
     int walls = density * 8; 
     int wallsd = rows * columns * density / 4; // formula to calculate the measurements of the labyrinth.
     for (int i = 0; i < rows; i++) // For each row, the number of columns will be created
-    {                                
+    {
+        matriz[i] = new int[columns]; // number of columns.
+    }
+    // definition of the values ​​of the columns and rows of the maze:
+    for (int a = 0; a < rows; a++)
+    {
+        for (int b = 0; b < columns ; b++)
+        {
+            if (a == 0 || b == 0 || a == rows-1  || b == columns-1)
+            {
+                matriz[a][b] = 1;
+            }
+            else
+            {
+                matriz[a][b] = 0;
+            }
+        }
+    }             
