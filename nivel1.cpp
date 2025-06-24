60f05ef40fad61c237729c9f48b77274b197b5ed
DianaAyala2 <00021325@uca.edu.sv>
2025-06-23 22:52:05 -0600

    menu

--- nivel1.cpp
+++ nivel1.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 
 #define MAP_WIDTH 50
-#define MAP_HEIGHT 15 
+#define MAP_HEIGHT 15
 #define DENSITY 150
 
+void showmenu();   // show menu to the user to continue
+void homescreen(); // game welcome
 
-void showmenu(); // show menu to the user to continue
-void homescreen(); //game welcome
-
-void gLabyrinth(WINDOW* win, int rows, int columns, int density, int **&matriz)
+void gLabyrinth(WINDOW *win, int rows, int columns, int density, int **&matriz)
 {
-    matriz = new int*[rows];
+    matriz = new int *[rows];
     int walls = density * 8;
     for (int i = 0; i < rows; i++)
         matriz[i] = new int[columns];
 
     // definition of the values ​​of the columns and rows of the maze:
-   for (int a = 0; a < rows; a++)
+    for (int a = 0; a < rows; a++)
         for (int b = 0; b < columns; b++)
-            matriz[a][b] = (a == 0 || b == 0 || a == rows-1 || b == columns-1) ? 1 : 0;
+            matriz[a][b] = (a == 0 || b == 0 || a == rows - 1 || b == columns - 1) ? 1 : 0;
 
     srand(time(NULL)); // It will be used to generate new values ​​for the rows or variables each time the program is run.
     for (int c = 0; c < density; c++)
@@ -43,16 +42,19 @@ void gLabyrinth(WINDOW* win, int rows, int columns, int density, int **&matriz)
             }
         }
     }
-// Draw the maze in the curses window (+1 to leave the border)
+    // Draw the maze in the curses window (+1 to leave the border)
     for (int e = 0; e < rows; e++)
         for (int f = 0; f < columns; f++)
             if (matriz[e][f] == 1)
-                mvwaddch(win, e+1, f+1, 178); // Pared
+                mvwaddch(win, e + 1, f + 1, 178); // Pared
             else
-                mvwaddch(win, e+1, f+1, ' '); // Espacio
+                mvwaddch(win, e + 1, f + 1, ' '); // Espacio
 }
 int main()
 {
+    showmenu();
+    homescreen();
+    //to show the menu and the homescreen to the player.
     initscr();
     noecho();
     cbreak();
@@ -64,7 +66,7 @@ int main()
     // create labyrinth window
     int startx = (COLS - columns) / 2;
     int starty = (LINES - rows) / 2;
-    WINDOW *lab_win = newwin(rows+2, columns+2, starty, startx);
+    WINDOW *lab_win = newwin(rows + 2, columns + 2, starty, startx);
     keypad(lab_win, TRUE); // enables arrows inlab_win
     box(lab_win, 0, 0);
     // generates and draws the labyrinth
@@ -73,31 +75,39 @@ int main()
 
     // Find a free cell for the character (in the maze matrix)
     int px = 1, py = 1;
-    for (int y = 1; y < rows-1; y++) {
-        for (int x = 1; x < columns-1; x++) {
-            if (laberinto[y][x] == 0) {
+    for (int y = 1; y < rows - 1; y++)
+    {
+        for (int x = 1; x < columns - 1; x++)
+        {
+            if (laberinto[y][x] == 0)
+            {
                 py = y;
                 px = x;
-                goto found; //to get out of two loops
+                goto found; // to get out of two loops
             }
         }
     }
 found:
-int ch;
+    int ch;
     nodelay(lab_win, TRUE);
 
-    while ((ch = wgetch(lab_win)) != 'q') {
+    while ((ch = wgetch(lab_win)) != 'q')
+    {
         // This will delete the character from the previous position.
-        mvwaddch(lab_win, py+1, px+1, ' ');
+        mvwaddch(lab_win, py + 1, px + 1, ' ');
 
         // Movement with arrows, only if the cell is not a wall
-        if (ch == KEY_UP    && laberinto[py-1][px] == 0) py--;
-        if (ch == KEY_DOWN  && laberinto[py+1][px] == 0) py++;
-        if (ch == KEY_LEFT  && laberinto[py][px-1] == 0) px--;
-        if (ch == KEY_RIGHT && laberinto[py][px+1] == 0) px++;
-    
+        if (ch == KEY_UP && laberinto[py - 1][px] == 0)
+            py--;
+        if (ch == KEY_DOWN && laberinto[py + 1][px] == 0)
+            py++;
+        if (ch == KEY_LEFT && laberinto[py][px - 1] == 0)
+            px--;
+        if (ch == KEY_RIGHT && laberinto[py][px + 1] == 0)
+            px++;
+
         // to draw the character in a new position
-        mvwaddch(lab_win, py+1, px+1, '@');
+        mvwaddch(lab_win, py + 1, px + 1, '@');
         wrefresh(lab_win);
 
         mvwprintw(lab_win, 0, 2, "Tecla: %d   ", ch);
@@ -112,7 +122,18 @@ int ch;
     delwin(lab_win);
     endwin();
     return 0;
-
-    
-
-
+}
+void (){
+    cout << "Welcome to Lost city :D" <<endl;
+    cout<< "press enter to continue" <<endl;
+}
+void showmenu(){
+    cout << "|-----------------------------|"<<endl;
+    cout << "|            MENU             |"<<endl;
+    cout << "| Select a option to continue |"<<endl;
+    cout << "|          1. Play            |"<<endl;
+    cout << "|       2. Show levels        |"<<endl;
+    cout << "|          3. Exit            |"<<endl;
+    cout << "|        4. Save game         |"<<endl;
+    cout << "|-----------------------------|"<<endl;
+}
