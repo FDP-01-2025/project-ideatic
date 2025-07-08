44aa532a5948fae144f2c1a9602cef9bfcf2d8ff
DianaAyala2 <00021325@uca.edu.sv>
2025-07-07 20:36:03 -0600

    mensajes nivel 3

--- src/message.h
+++ src/message.h
@@ -3,7 +3,7 @@
 
 #include <curses.h>
 
-//mensaje de introducción al nivel 1
+// mensaje de introducción al nivel 1
 void level1message(WINDOW *win)
 {
     wclear(win); // Limpia la ventana
@@ -33,5 +33,93 @@ void level1message(WINDOW *win)
     wclear(win); // Limpia la ventana al final
     wrefresh(win);
     napms(150); // Pausa final
+}
+void level3message(WINDOW *win)
+{
+
+    wclear(win);
+    wrefresh(win);
+    string message1 = "Congratulations! You've made it to level 3 :D";
+    string message2 = "Now you must defeat the enemy of this level to move on to the last one.";
+    string message3 = "Good luck!, see you at the next level";
+
+    string message[] = {message1, message2, message3};
+
+    for (int i = 0; i < 3; i++)
+    {
+        wprintw(win, "\n");
+        for (char a : message[i])
+        {
+            wprintw(win, "%n", a);
+            wclear(win);
+            napms(20);
+        }
+        napms(800);
+    }
+    wclear(win);
+    wrefresh(win);
+    napms(150);
+}
+void loading(WINDOW *win)
+{
+    wclear(win);
+    wrefresh(win);
+    string message = "loading";
+    string message1 = "...";
+    string message2[] = {message, message1};
+
+    for (int j = 0; j < 2; j++)
+    {
+        for (char c : message2[j])
+        {
+            wprintw(win, "%n", c);
+            napms(50);
+        }
+    }
+    napms(300);
+    wclear(win);
+    wrefresh(win);
+}
+void finalmessagelevel1(WINDOW * win)
+{
+    napms(1000);
+    wclear(win);
+    string message = "You've completed the level";
+    string message1 = "but don't declare victory yet!";
+    string message2 = "Press Z to move on to the next level";
+    string message3[] = {message, message1, message2};
+    for (int i = 0; i < 3; i++)
+    {
+        cout << endl;
+        for (char b : message3[i])
+        {
+            wprintw(win, "%n", b);
+            napms(50);
+        }
+        napms(300);
+    }
+    napms(300);
+    wclear(win);
+}
+void finalmessage2(WINDOW* win)
+{
+    napms(500);
+    wclear(win);
+    string message = "You lost :(";
+    string message1 = "Try again to move on to the next level";
+    string message2 = "Don't get discouraged :D!";
+    string message3[] = {message, message1, message2};
+    for (int i = 0; i < 3; i++)
+    {
+        cout << endl;
+        for (char b : message3[i])
+        {
+            wprintw(win, "%n", b);
+            napms(50);
+        }
+        napms(300);
+    }
+    napms(500);
+    wclear(win);
 }
 #endif // MESSAGE_H
